d4cba1ddbc6a3e570e13ada64425d374afd95711
Rash6 <rashmirohini20@gmail.com>
2022-06-20 20:07:00 +0530

    listenig to server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 // install and import express
-const express = () => {};
+const express = require("express")
 let app = express();
+app.use(express.json())
+var file=require("fs");
+const filejson="./assets/user.json";
+const file_json=require(filejson)
+
 
 // Code here
+app.listen(8000,async()=>{
+    try{
+        console.log("port 8000 is live")
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
 
 // Note: Do not remove this export statement
 module.exports = app;
